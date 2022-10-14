cb299845d41d2d104298cba24ecd50b6e48134c8
williamtran207 <quochuyphbrvt07@gmail.com>
2022-10-14 20:33:03 +0700

    upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Ngọc My';
-const giftUrl = 'https://www.facebook.com/nqocmecutes1tg';
+const nameGirl = 'Trà My';
+const giftUrl = 'https://www.facebook.com/tzamizz1510';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng em Ngọc My';
-const contentCard = 'Chúc My sinh nhật tràn ngập niềm vui. Chúc em gặp nhiều may mắn, học giỏi hơn, đạt học sinh giỏi trong năm học này và đỗ vào trường cấp 3 như ý nha kkk';
+const titleCard = 'Tặng Trà My';
+const contentCard = 'Chúc My một sinh nhật tràn ngập niềm vui. Chúc bà gặp nhiều may mắn, học giỏi hơn, đạt học sinh giỏi trong năm học và đỗ vào trường cấp 3 như ý nha kkk';
 const giftImage = 'honey.JPG';
 const base64 = '';
 const giftImageBase64 = "data:image/jpeg;base64, " + base64;
