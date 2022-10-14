66f64ffb5cd195fd7a23130e9ee7124559cd1e3d
williamtran207 <quochuyphbrvt07@gmail.com>
2022-10-14 20:36:06 +0700

    upload

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const giftUrl = 'https://www.facebook.com/tzamizz1510';
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Tặng Trà My';
 const contentCard = 'Chúc My một sinh nhật tràn ngập niềm vui. Chúc bà gặp nhiều may mắn, học giỏi hơn, đạt học sinh giỏi trong năm học và đỗ vào trường cấp 3 như ý nha kkk';
-const giftImage = 'honey.JPG';
+const giftImage = 'tramy.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/jpeg;base64, " + base64;
