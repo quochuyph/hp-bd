4da27119d22fe72e4f270b513bd0554127b3c36d
williamtran207 <83764817+trhuy207@users.noreply.github.com>
2023-06-06 23:15:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const giftUrl = 'https://www.facebook.com/profile.php?id=100091636334386&mibexti
 const eventName = 'Chúc Mừng Sinh Nhật';
 const titleCard = 'Tặng Chị Trang';
 const contentCard = 'Chúc Chị một sinh nhật tràn ngập niềm vui. Chúc Chị gặp nhiều may mắn, học giỏi hơn, ngày càng xinh đẹp hơn nha kkk';
-const giftImage = 'chitrang.jpg';
+const giftImage = 'a0f25d247256690572e4d126a34b5115.jpeg';
 const base64 = '';
 const giftImageBase64 = "data:image/jpeg;base64, " + base64;
