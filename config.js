1c95cfc1d1d1adf8db3be994a117ee1dd308e7ad
williamtran207 <83764817+quochuyph@users.noreply.github.com>
2025-03-14 08:40:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Bạn Kha';
-const giftUrl = 'https://www.facebook.com/profile.php?id=61553783829230';
+const nameGirl = 'Bạn Tuyết Nhung';
+const giftUrl = 'https://www.facebook.com/tran.thi.tuyet.nhung.101365';
 const eventName = 'Chúc Mừng Sinh Nhật';
-const titleCard = 'Tặng Bạn Kha';
-const contentCard = 'Chúc Bà một sinh nhật tràn ngập niềm vui. Chúc Bà gặp nhiều may mắn, học ngày càng giỏi hơn, ngày càng xinh đẹp hơn, và Đỗ NV 1 nhaaaaa';
+const titleCard = 'Tặng Bạn Nhung';
+const contentCard = 'Chúc bạn một sinh nhật tràn ngập niềm vui. Chúc bạn gặp nhiều may mắn, học ngày càng giỏi hơn, ngày càng xinh đẹp hơn, và Đỗ NV 1 nhaaaaa';
 const giftImage = '1.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/jpg;base64, " + base64;
